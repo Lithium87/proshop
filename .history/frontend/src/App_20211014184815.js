5a9300a8a280d0850e5e7de4fb77b2f51db447b6
Lithium@87 <cibs@abv.bg>
2021-10-14 18:52:39 +0300

    Create userReducer, userAction and RegisterScreen

--- .history/frontend/src/App_20211014184815.js
+++ .history/frontend/src/App_20211014184815.js
@@ -5,6 +5,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
@@ -12,8 +15,11 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
           <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart/:id?" component={CartScreen} />
+          <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
       <Footer />
