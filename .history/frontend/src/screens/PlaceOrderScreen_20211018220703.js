f9c06f7ba28e82216084a26650bced17ce210fe9
Lithium@87 <cibs@abv.bg>
2021-10-18 23:48:51 +0300

    Create PlaceOrderScreen

---
+++ .history/frontend/src/screens/PlaceOrderScreen_20211018220703.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Button, Row, Col, ListGroup, Image, Card} from 'react-bootstrap';
+import Message from '../components/Message';
+import CheckoutSteps from '../components/CheckoutSteps';
+
+const PlaceOrderScreen = () => {
+    const cart = useSelector(state => state.cart);
+
+    return (
+        <>
+            <CheckoutSteps step1 step2 step3 step4 />
+            <Row>
+                <Col md={8}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Shipping</h2>
+                            <p>
+                                <strong>Address: </strong>
+                                {cart.shippingAddress.address}, 
+                                {cart.shippingAddress.city} 
+                                {cart.shippingAddress.postalCode},
+                                {cart.shippingAddress.country}
+                            </p>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <h2>Payment Method</h2>
+                            <strong>Method: </strong>
+                            {cart.paymentMethod}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default PlaceOrderScreen;
