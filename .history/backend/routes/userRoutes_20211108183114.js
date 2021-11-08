985bec12eff59ad77a3e73bbcfe052e1ca9dc67a
Lithium@87 <cibs@abv.bg>
2021-11-08 18:40:37 +0200

    Create admin middleware and implement getUsers functionality on the server

--- .history/backend/routes/userRoutes_20211108183114.js
+++ .history/backend/routes/userRoutes_20211108183114.js
@@ -3,13 +3,18 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
+  getUsers,
 } from '../controllers/userController.js';
 import {protect} from '../middleware/authMiddleware.js';
 
 const router = express.Router ();
 
-router.route ('/').post (registerUser);
+router.route ('/').post (registerUser).get (protect, getUsers);
 router.post ('/login', authUser);
-router.route ('/profile').get (protect, getUserProfile);
+router
+  .route ('/profile')
+  .get (protect, getUserProfile)
+  .put (protect, updateUserProfile);
 
 export default router;
